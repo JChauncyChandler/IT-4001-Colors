8e5ea8a681115d0f3679155e53a0445a2d5ac8b4
Jackson Chandler <jackson.c.chandler@gmail.com>
2017-10-27 09:14:43 -0500

    Added navigation

---
+++ ColorsTable/ColorsTable/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  ColorsTable
+//
+//  Created by Chandler, Jackson C. (MU-Student) on 10/27/17.
+//  Copyright © 2017 Chandler, Jackson C. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
